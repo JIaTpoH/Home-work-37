e846fd70c3e7c746c519545fa32bbd878c5760a0
Dmitri Mandaji <badmopss@gmail.com>
2023-03-10 14:52:12 +0100

    add the get

--- src/Rook.java
+++ src/Rook.java
@@ -1,2 +1,21 @@
 public class Rook {
+  public enum Color {
+    WHITE,
+    BLACK,
+  }
+  private Color color;
+  private int row;
+  private int column;
+  public Rook(Color color,int row, int column) {
+    this.color = color;
+    ckeckCoordinates(row,column);
+    this.row = row;
+    this.column = column;
+  }
+  public int getRow() {
+    return row;
+  }
+  public int getColumn() {
+    return column;
+  }
 }
