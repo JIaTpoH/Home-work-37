3a41a27101c88cff2d9deee4346fa054f9042f55
Dmitri Mandaji <badmopss@gmail.com>
2023-03-10 14:58:43 +0100

    add the checkCoordinates

--- src/Rook.java
+++ src/Rook.java
@@ -8,7 +8,7 @@ public class Rook {
   private int column;
   public Rook(Color color,int row, int column) {
     this.color = color;
-    ckeckCoordinates(row,column);
+    checkCoordinates(row, column);
     this.row = row;
     this.column = column;
   }
@@ -20,5 +20,15 @@ public class Rook {
   }
   public void setCoordinates(int row, int column) {
     checkCoordinates(row,column);
+  }
+  private void checkCoordinates(int row, int column) {
+    if (row < 1 || row > 8) {
+      throw new IllegalArgumentException("Некорректный номер строки: " + row);
+    }
+
+    if (column < 1 || column > 8) {
+      throw new IllegalArgumentException("Некорректный номер столбца: " + column);
+
+    }
   }
 }
